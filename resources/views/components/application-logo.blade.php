5b7197b03d3588196f471a28d308ab54237e9340
Willy Indonesia <willyindonesia94@example.com>
2026-09-17 11:20:00 +0700

    UI: sesuaikan warna teks logo dengan warna biru dan kuning khas PLN

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'text-2xl font-bold tracking-tight text-blue-800']) }}>
-    <span class="text-yellow-500">4DX</span> PLN UID JABAR
+<div {{ $attributes->merge(['class' => 'text-2xl font-bold tracking-tight text-[#00a2e9]']) }}>
+    <span class="text-[#eab308]">4DX</span> PLN UID JABAR
 </div>
