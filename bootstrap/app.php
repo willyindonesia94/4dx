26624520c39e15b1c301d8acf3217b558ca6df27
Willy Indonesia <willyindonesia94@example.com>
2026-09-21 10:27:29 +0700

    Add global middleware to forcefully logout disabled accounts that have active sessions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             \App\Http\Middleware\LogActivityMiddleware::class,
+            \App\Http\Middleware\CheckUserActive::class,
         ]);
         
         $middleware->alias([
